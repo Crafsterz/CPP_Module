d1b71b124c53f76e6dc988b7f7414bed06e6dfa7
Replit user <>
2024-02-27 15:12:59 +0000

    ex05 nearly complete. main needs update

---
+++ cpp01/ex05/Harl.cpp
@@ -0,0 +1,38 @@
+#include "Harl.hpp"
+
+Harl::Harl()
+{}
+
+Harl::~Harl(void)
+{}
+
+void	Harl::debug(void)
+{
+  cout << "The solution has been met" << endl;
+}
+
+void	Harl::info(void)
+{
+  cout << "The answers were simple and well met. The calcuations were correct." << endl;
+}
+
+void	Harl::warning(void)
+{
+  cout << "There seems to be a problem with the solution. Particular factors are missing to the calculations." << endl;
+}
+
+void	Harl::error(void)
+{
+  cout << "UNSOLVABLE! The calculations and each permusable factor are wrong!" << endl;
+}
+
+void complain(string level)
+{
+  t_func funcs[] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+  string levels[] = {"debug", "info", "warning", "error"};
+  int i = 0;
+  while (i < 4 && levels[i].compare(level))
+      i++ ;
+  if (i < 4)
+      (this->*funcs[i])();
+}
---
+++ cpp01/ex05/Harl.hpp
@@ -0,0 +1,26 @@
+#ifndefine HARL_HPP
+# define HARL_HPP
+# include <iostream>
+# include <string>
+using std::string;
+using std::cout;
+using std::endl;
+
+class Harl
+{
+  private:
+    void  debug(void);
+    void  info(void);
+    void  warning(void);
+    void  error(void);
+
+  public:
+    Harl();
+    ~Harl(void);
+    void  complain(string level);
+
+}
+
+typedef void (Harl::*t_func) ( void );
+
+#end
