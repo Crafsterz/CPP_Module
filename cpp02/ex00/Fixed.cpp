8c3fc78c119b9235f885ca4a7f25dc7d32071e8c
Muhammad Fikrie Khairul Muhsein <mukhairu@u91z01s02.42kl.edu.my>
2024-03-07 18:28:25 +0800

    cpp02 added

---
+++ cpp02/ex00/Fixed.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mukhairu <mukhairu@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 15:20:03 by mukhairu          #+#    #+#             */
+/*   Updated: 2024/03/07 17:17:45 by mukhairu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+const int	Fixed::fractBits = 8;
+
+Fixed::Fixed(/* args */)
+{
+	cout << "Default constructor called" << endl;
+	fixedNum = 0;
+}
+
+Fixed::Fixed(const Fixed& copy)
+{
+	cout << "Copy constructor called" << endl;
+	*this = copy;
+}
+
+Fixed &Fixed::operator = (const Fixed& src)
+{
+	cout << "Copy assignment operator called" << endl;
+	this->fixedNum = src.getRawBits();
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const
+{
+	cout << "getRawBits member function called" << endl;
+	return (fixedNum);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	cout << "setRawBit member function called" << endl;
+	this->fixedNum = raw;
+}
+
+Fixed::~Fixed(void)
+{
+	cout << "Deconstructor called" << endl;
+}
---
+++ cpp02/ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mukhairu <mukhairu@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 15:20:07 by mukhairu          #+#    #+#             */
+/*   Updated: 2024/03/07 17:10:42 by mukhairu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main (void)
+{
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return (0);
+}
+
